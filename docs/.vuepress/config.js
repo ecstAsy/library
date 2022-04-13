d2cfce7b397deb19867c04e086bc7fad65dc795b
ecstAsy <sky_hmn@163.com>
2022-04-13 17:19:12 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ecstAsy
  * @Date: 2022-01-04 17:50:58
- * @LastEditTime: 2022-04-13 17:08:34
+ * @LastEditTime: 2022-04-13 17:18:27
  * @LastEditors: ecstAsy
  */
 module.exports = {
@@ -199,7 +199,10 @@ module.exports = {
       }, {
         title: '请求模板',
         path: '/library/taro/taro-request'
-      },  {
+      }, {
+        title: '渲染富文本',
+        path: '/library/taro/taro-richtext'
+      }, {
         title: 'Weapp H5 踩坑',
         path: '/library/taro/taro-weapp-h5'
       }, ]
